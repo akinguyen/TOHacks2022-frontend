3224c69f6702fb8ae1f70b4a0e50b82f83325baa
Duy Nguyen <duynguyenquykhanh@gmail.com>
2022-05-28 22:41:38 -0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,21 @@
-import React from "react";
-
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../states/Provider";
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const [state, dispatch] = useContext(Context);
+
+  if (!state.isLoggedIn) {
+    window.location.href = "/login";
+    return undefined;
+  }
+
+  return (
+    <div className="home-background">
+      <div className="nav-bar">
+        <div>Helping Pop Up Businesses Thrive During Pandemic</div>
+        <button>Log Out</button>
+      </div>
+    </div>
+  );
 }
